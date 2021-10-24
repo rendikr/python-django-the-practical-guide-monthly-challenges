ed7ff4b658468bb97490ef13450d4e9d863c6641
rendikr <rendi.krinaldi@gmail.com>
2021-10-24 11:55:22 +0700

    23. dynamic path segments & captured values

--- challenges/views.py
+++ challenges/views.py
@@ -11,3 +11,21 @@ def january(request):
 
 def february(request):
     return HttpResponse("Walk for at least 20 minutes every day!")
+
+
+def march(request):
+    return HttpResponse("Hello There Handsome!")
+
+
+def month_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'This Works!'
+    elif month == 'february':
+        challenge_text = 'Walk for at least 20 minutes every day!'
+    elif month == 'march':
+        challenge_text = 'Hello there handsome!'
+    else:
+        return HttpResponse('This month is not supported!')
+
+    return HttpResponse(challenge_text)
