694ece89fd81deb1b6c4d1786b754f0584d4ac85
Rendi K <rendi.krinaldi@gmail.com>
2021-10-24 16:40:35 +0700

    29. practicing urls, views, & dynamic view logic

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.month_challenge, name="month-challenge")
+    path("", views.index), # /challenges/
+    path("<int:month>", views.monthly_challenge_by_number), # /challenges/1
+    path("<str:month>", views.month_challenge, name="month-challenge") # /challenges/january
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -20,6 +20,18 @@ monthly_challenges = {
 
 # Create your views here.
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        month_path = reverse("month-challenge", args=[month])
+        capitalized_month = month.capitalize()
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
